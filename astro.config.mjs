49d9b3b2ad71a45192fb85dca3baac4c32408694
chiboy-star <chinemeremmadueke1@gmail.com>
2026-01-02 19:19:11 +0100

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. "integrations" is for Astro-specific packages (like Icons)
+  integrations: [
+    icon()
+  ],
+  
+  // 2. "vite" is for the new Tailwind v4 engine
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
